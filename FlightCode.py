93e54704ae70d7756c8502c172de2f51a2b0a909
Jodick Ndayisenga <rajajodick@gmail.com>
2022-11-05 11:16:25 +0300

    flight

--- FlightCode.py
+++ FlightCode.py
@@ -1,20 +1,25 @@
-#the code will check if there is any place available on plane and add if yes
+# the code will check if there is any place available on plane and add if yes
 
 class Flight():
-    def __init__(self,capacity):
+    def __init__(self, capacity):
         self.capacity = capacity
         self.people = []
 
     def check_place(self):
         per = input('what is your name? ')
-        if len(self.people)<=5:
+        if len(self.people) < 5:
             self.add_passenger(per)
-            print(self.people)
+            for name in self.people:
+                print(f'We have on the flight: {name}')
+            print(f'And we have only {5-len(self.people)} places available')
+            # print(self.people)
         else:
-            print(f'I am sorry, we could not add ,{per}')
+            print(f'I am sorry, we could not add {per}')
+            quit()
 
-    def add_passenger(self,x):
+    def add_passenger(self, x):
         self.people.append(x)
+
 
 flight = Flight(5)
 while True:
